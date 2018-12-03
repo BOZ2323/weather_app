63da1c1c061b30d46dc98b565e87d6fb01157fee
Lilli <lillibaaske@gmx.de>
2018-12-03 17:33:21 +0100

    receiving weather data

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import React from 'react';
+import Titles from './components/Titles';
+import Form from './components/Form';
+import Weather from './components/Weather';
 
-
+const API_KEY = '344553880e500887d7d24b966f0e73a2';
 
 class App extends React.Component{
+    getWeather= async (e) => {
+        e.preventDefault();
+        const city = e.target.elements.city.value;
+        const country = e.target.elements.country.value;
+        const api_call = await fetch(`http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=${API_KEY}`);
+        const data = await api_call.json();
+        console.log(data);
+    }
+
+
     render(){
         return(
             <div>
-            <p>hello</p>
-            <h1>YO!</h1>
+                <Titles/>
+                <Form getWeather = {this.getWeather}/>
+                <Weather/>
+
             </div>
         );
     }
---
+++ src/components/Titles.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+class Titles extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1>Weather finder</h1>
+                <p>find out</p>
+            </div>
+        );
+
+    }
+}
+
+export default Titles;
